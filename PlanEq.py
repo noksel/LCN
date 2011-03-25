c00603e1601135c1408478bdcd2a48b8b4141655
noksel <noksel.ya@yandex.ru>
2011-03-25 11:34:22 +0300

    baga

--- PlanEq.py
+++ PlanEq.py
@@ -9,31 +9,29 @@ class PlanEq(db.Model):
 	idEquipment=db.ReferenceProperty(equipment.Equipment)
 	quantity=db.IntegerProperty()
 	comment=db.StringProperty()
-	resp=db.ListProperty(str)
+	resp=db.ListProperty(unicode)
 	
 class PlanEqPage(webapp.RequestHandler):
 	def get(self):
 		self.response.out.write("""<html><body>""")
 		peqs=db.GqlQuery('SELECT * FROM PlanEq')
-		db.delete(peqs)
+		#db.delete(peqs)
 		if peqs.count()==0:
 			self.init()
 			peqs=db.GqlQuery('SELECT * FROM PlanEq')
 	
 		self.response.out.write(u"""<table border="1">
 														<tr><th>Название</th><th>Колличество</th><th>Комментарий</th><th>Ответственные</th></tr>""")
-		for peq in peqs:
-			
+		for peq in peqs:			
 			self.response.out.write("<tr> <td>%s</td> <td>%s</td><td>%s</td>" % (peq.idEquipment.name,peq.quantity,peq.comment))
 			mstr=unicode()
 			self.response.out.write("<td>")
-			tmp=[u'd']
 			for wrkey in peq.resp:
 				mstr=mstr+"%s "%db.get(wrkey).surname		
 			
 			
 			
-			self.response.out.write("%s</td></tr>" % mstr)							
+		self.response.out.write("%s</td></tr>" % mstr)							
 		
 		self.response.out.write('</table>')
 		self.response.out.write("""</body></html>""")
--- PlanEqAdd.py
+++ PlanEqAdd.py
@@ -12,15 +12,9 @@ class PEAdd(webapp.RequestHandler):
 		pe.idEquipment=db.get(self.request.get('eqid'))
 		pe.quantity=int(self.request.get('quant'))
 		pe.comment=self.request.get('comment')
-		self.response.out.write("%s _ %s _ %s"%(pe.idEquipment.name,pe.quantity,pe.comment))
 		
-		#self.response.out.write("%s _ %s _ "%(pe.quantity,pe.comment))
 		
-		#pe.resp=[self.request.get('resp')]
-		d=self.request.get('resp').split(':')
-		
-		for wk in d:
-			self.response.out.write("%s\n"%db.get(wk).surname)
-		self.response.out.write(d)
-		
-		#pe.put()
+		pe.resp=self.request.get('resp').split(':')
+	
+		pe.put()
+		self.redirect('/planeq')
