d5d44f8b8f469e4a015c2943fe382089387a3925
noksel <noksel.ya@yandex.ru>
2011-04-08 00:03:47 +0400

    fix lcncss.py

--- lcncss.py
+++ lcncss.py
@@ -1,23 +1,6 @@
 # -*- coding: UTF-8 -*-
 class Mtempl:
-	beg=u"""
-			<div id="tble">
-			<div id="caption">Заголовок</div>
-			<div class="tr">
-				<div id="lft">
-				<hr>
-				<a href="/workers">Сотрудники</a><br/><hr>
-				<a href="/equipment">Оборудование</a><br/><hr>
-				<a href="/planeq">План закупок по оборудованию</a><br/><hr>
-				<a href="/order">Заявки</a><hr>
-				<hr>
-			
-				<a href="/tpaymnt">Типы платежей</a><br/>
-				<a href="/vendor">Поставщики</a><br/>
-				<a href="/payer">Плательщики</a><br/>
-				</div>	
-				<div id="mddle">	
-			"""
+
 	end=u"""</div></div></div>"""
 def beg(name):
 		b1=u"""
